2e4057644d27512660f15e76154eef3b3e9bda0a
Miłosz Jaśniok <milosz.jasniok@xsolve.pl>
2016-04-18 14:24:55 +0200

    +lambda exercises

---
+++ java8-lambda/src/test/java/pl/xsolve/workshops/java8/lambda/Java8Solutions.java
@@ -0,0 +1,7 @@
+package pl.xsolve.workshops.java8.lambda;
+
+public class Java8Solutions extends LambdaExercisesTest {
+    public Java8Solutions() {
+        super(new LambdaExercisesJava8SolutionsImpl());
+    }
+}
---
+++ java8-lambda/src/test/java/pl/xsolve/workshops/java8/lambda/OldJava.java
@@ -0,0 +1,7 @@
+package pl.xsolve.workshops.java8.lambda;
+
+public class OldJava extends LambdaExercisesTest {
+    public OldJava() {
+        super(new LambdaExercisesOldJavaImpl());
+    }
+}
